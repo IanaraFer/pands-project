b9f7d6dce9e13191c606679933da6522fd92d0a0
Ianara Fernandes <ianarafer@gmail.com>
2024-05-08 13:41:13 +0100

    starting analyse dataset

--- analysis.py
+++ analysis.py
@@ -1,14 +1,4 @@
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-iris = fetch_ucirepo(id=53) 
-  
-# data (as pandas dataframes) 
-X = iris.data.features 
-y = iris.data.targets 
-  
-# metadata 
-print(iris.metadata) 
-  
-# variable information 
-print(iris.variables) 
+# Exploring Iris DataSet using Pandas
+
+import pandas as pd
+
