bec4d51d57c30ec56d9418036e371744cb3b7995
Ianara Fernandes <ianarafer@gmail.com>
2024-05-08 14:44:44 +0100

    python dataset csv results and notebook

--- analysis.py
+++ analysis.py
@@ -7,3 +7,5 @@ df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 
 df['species'] = iris.target_names[iris.target]
 df.to_csv('iris_dataset.csv', index=False)
+# Showing the dataset csv
+print(df)
