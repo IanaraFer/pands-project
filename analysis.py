c515be9950f24023fc32f8c4c9784cb3622fb56e
Ianara Fernandes <ianarafer@gmail.com>
2024-05-08 14:14:39 +0100

    saving the dataset in csv

--- analysis.py
+++ analysis.py
@@ -1,4 +1,10 @@
 # Exploring Iris DataSet using Pandas
 
-import pandas as pd
+from sklearn import datasets
+iris = datasets.load_iris()
 
+import pandas as pd
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+df['species'] = iris.target_names[iris.target]
+df.to_csv('iris_dataset.csv', index=False)
