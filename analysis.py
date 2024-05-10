d1597a5776fe28ebdfa5d1f576aea1b0329a7013
Ianara Fernandes <ianarafer@gmail.com>
2024-05-10 12:58:26 +0100

    create histogram of iris species one which and after all together

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 # Exploring Iris DataSet using Pandas
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import datasets
 iris = datasets.load_iris()
 
@@ -19,3 +20,64 @@ df.isnull().sum()
 # Counts the difirent species.
 
 df['species'].value_counts()
+
+# Histogram
+
+plt.figure(figsize=(10, 7))
+x = iris.data[:, 0]  # Sepal Length
+plt.hist(x, bins=20, color="green")
+plt.title("Sepal Length in cm")
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Count")
+plt.show()
+
+plt.figure(figsize=(10, 7))
+x = iris.data[:, 1]  # Sepal Width
+plt.hist(x, bins=20, color="black")
+plt.title("Sepal Width in cm")
+plt.xlabel("Sepal Width (cm)")
+plt.ylabel("Count")
+plt.show()
+
+plt.figure(figsize=(10, 7))
+x = iris.data[:, 2]  # Petal Length
+plt.hist(x, bins=20, color="blue")
+plt.title("Petal Length in cm")
+plt.xlabel("Petal Length (cm)")
+plt.ylabel("Count")
+plt.show()
+
+plt.figure(figsize=(10, 7))
+x = iris.data[:, 3]  # Petal Width
+plt.hist(x, bins=20, color="red")
+plt.title("Petal Width in cm")
+plt.xlabel("Petal Width (cm)")
+plt.ylabel("Count")
+plt.show()
+
+# Create subplots for all four species
+plt.figure(figsize=(10, 7))
+x = iris.data[:, 0]  # Sepal Length
+plt.hist(x, bins=20, color="green")
+plt.title("Sepal Length in cm")
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Count")
+
+x = iris.data[:, 1]  # Sepal Width
+plt.hist(x, bins=20, color="black")
+plt.title("Sepal Width in cm")
+plt.xlabel("Sepal Width (cm)")
+plt.ylabel("Count")
+
+x = iris.data[:, 2]  # Petal Length
+plt.hist(x, bins=20, color="blue")
+plt.title("Petal Length in cm")
+plt.xlabel("Petal Length (cm)")
+plt.ylabel("Count")
+
+x = iris.data[:, 3]  # Petal Width
+plt.hist(x, bins=20, color="red")
+plt.title("Petal Width in cm")
+plt.xlabel("Petal Width (cm)")
+plt.ylabel("Count")
+plt.show()
